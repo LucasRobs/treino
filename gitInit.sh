d281817d15842c3a006e413a98a33e59d9f94f4b
LucasRobs <lucasrobs@alu.ufc.br>
2020-06-25 15:11:19 -0300

    teste

---
+++ gitInit.sh
@@ -0,0 +1,6 @@
+git init
+git config user.name "LucasRobs"
+git config user.email "lucasrobs@alu.ufc.br"
+echo "Nome do repositorio: "
+read repositorio
+git remote add origin https://github.com/LucasRobs/$repositorio
